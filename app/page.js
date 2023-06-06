f674c33098bc31c296cc937c3b3c0c5883cf74dc
jamesdosstech <jamesdosstech@gmail.com>
2023-06-06 16:43:55 -0500

    create product item

--- app/page.js
+++ app/page.js
@@ -1,10 +1,18 @@
 import Layout from "@/components/Layout";
 import RootLayout from "./layout";
+import { data } from "@/utils/data";
+import ProductItem from "@/components/ProductItem";
 
 export default function Home() {
   return (
     <div>
-      <Layout>Home Page</Layout>
+      <Layout>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+          {data.products.map((product) => (
+            <ProductItem product={product} key={product.slug}></ProductItem>
+          ))}
+        </div>
+      </Layout>
     </div>
   );
 }
---
+++ components/ProductItem.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+
+function ProductItem({ product }) {
+  return (
+    <div className="card">
+      <Link href={`/product/${product.slug}`}>
+        <img
+          src={product.image}
+          alt={product.name}
+          className="object-cover rounded shadow"
+        ></img>
+      </Link>
+      <div className="flex flex-col items-center justify-center">
+        <Link href={`/product/${product.slug}`}>
+          <h2 className="text-lg">{product.name}</h2>
+        </Link>
+        <p className="mb-2">{product.name}</p>
+        <p>${product.price}</p>
+        <button className="primary-button" type="button">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ProductItem;
